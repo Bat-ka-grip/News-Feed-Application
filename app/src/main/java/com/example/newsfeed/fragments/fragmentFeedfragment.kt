ef9c255cccaa735e0ed49e0a79a8c83d098b57f1
vishwas <vishwasbabar886@gmail.com>
2023-10-12 23:50:17 +0530

    added some ui

--- app/src/main/java/com/example/newsfeed/fragments/fragmentFeedfragment.kt
+++ app/src/main/java/com/example/newsfeed/fragments/fragmentFeedfragment.kt
@@ -6,10 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
 import com.example.newsfeed.MainActivity
 import com.example.newsfeed.MainActivity2
 import com.example.newsfeed.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class fragmentFeedfragment : Fragment() {
     // TODO: Rename and change types of parameters
@@ -28,6 +31,13 @@ class fragmentFeedfragment : Fragment() {
 
             val intent = Intent(activity, MainActivity2::class.java)
             startActivity(intent)
+        }
+
+        // this code is for to show the snackbar when user click save for later icon
+        val imageButton = rootView.findViewById<ImageButton>(R.id.save_later_button)
+        imageButton.setOnClickListener(){
+            Snackbar.make(rootView, "Saved for Later.", Snackbar.LENGTH_SHORT).show()
+//            Toast.makeText(requireContext() , "saved for watch later", Toast.LENGTH_SHORT).show()
         }
 
         return rootView
--- app/src/main/java/com/example/newsfeed/fragments/fragmentProfilefragment.kt
+++ app/src/main/java/com/example/newsfeed/fragments/fragmentProfilefragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import com.example.newsfeed.R
+import com.google.android.material.snackbar.Snackbar
 
 class fragmentProfilefragment : Fragment() {
     // TODO: Rename and change types of parameters
@@ -16,7 +18,23 @@ class fragmentProfilefragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profilefragment, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_profilefragment, container, false)
+
+        val imageButton_notification = rootView.findViewById<ImageButton>(R.id.profile_notification_button)
+
+        //creating a object to call the java methods
+        val java_obj = fragmentProfilefragment_java()
+//        java_obj.showActionbar(imageButton_notification, "Notification is on for this profile", requireView())
+
+        java_obj.main()
+//        java_obj.showActionbar("vishwas babar", requireView())
+
+        imageButton_notification.setOnClickListener(){
+            Snackbar.make(requireView(), "Notification is on.", Snackbar.LENGTH_SHORT ).show()
+        }
+
+
+        return rootView
     }
 
 
